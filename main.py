8c931e253b0ac9099c61d772de02e9b3d1bb7b96
Cristhian Lucio Quispe Nina <102777824+SCRISTHIAN@users.noreply.github.com>
2023-05-29 17:08:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import asyncio
 from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import pymysql
 import aiomysql
 from contextlib import asynccontextmanager
-from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 # jwt = JWTManager(app)
